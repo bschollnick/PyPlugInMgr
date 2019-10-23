7225b475f06a5111e0fcef34534b431b45cc819d
Benjamin Schollnick <benjamin@schollnick.net>
2019-10-23 08:45:02 -0400

    Initial v0.501 load
    Initial v0.501 load

---
+++ Plugins/test2.py
@@ -0,0 +1,8 @@
+__version__ = "0.5"
+__author__ = "Benjamin Schollnick"
+__status__ = "Beta"
+__credits__ = ["Benjamin Schollnick"]
+__maintainer__ = "Benjamin Schollnick"
+__email__ = "Benjamin@Schollnick.net"
+__AppName__ = 'Plugin Manager'
+__PluginName__ = "Test2.py"
