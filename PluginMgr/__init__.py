95e2fbf1b92a249c7432eb71255e124bf5d247ea
Benjamin Schollnick <benjamin@schollnick.net>
2020-05-26 18:16:55 -0400

    Update __init__.py

--- PluginMgr/__init__.py
+++ PluginMgr/__init__.py
@@ -1,5 +1,5 @@
 """
-General Plugin Manager for Python
+General Plugin Manager for Python v3
 
 The basic concept is a simplified (but not less featured) version of YAPSY.
 
@@ -8,26 +8,56 @@ https://lkubuntu.wordpress.com/2012/10/02/writing-a-python-plugin-api/.  Others
 were not...
 
 YAPSY, and I have had a distinct love/hate relationship, the main issue is that
-in the application I am using YAPSY with, I have an issue with YAPSY not
-consistently working reliably.
+in the application I am using YAPSY with plugins sometimes stop functioning for
+no obvious reason.
 
-PPM is designed around the importlib utilities, it is using core functionality
-so all you need to do is create a module, and place it in a directory.
+While YAPSY, and PPM are both designed around "Python's standard library'".
+The main difference is simply in execution.  PPM does not require configuration
+files, and allows the implementor to design the code of the plug-in to work
+without needing an dedicated "plug-in" class.
 
+Example:
 
 .. code-block:
     import PyPluginMgr
-    test = PyPluginMgr.PlugInMgr(plugin_dir=r".\plugins",
+    test = PyPluginMgr.PlugInMgr(plugin_dir=r"plugins",
                                  allow_creation=True, plug_ext=".py")
     test.findcandidate_files()
 
 at this point, PPM will check the plugin_dir for files, if the directory does
-not exist (allow_creation) it would be created.  Please note plug_ext must be
-.py.  I have not investigated myself, but comments in the stackoverflow thread
-(see findcandidate_files) indicate this import mechanism does not allow
-non-standard (.py) file extensions.
+not exist (allow_creation=True) it would be created.
 
-Testing does bare this out.
+Please note plug_ext must be .py.  The comments in the stackoverflow thread
+(see findcandidate_files) indicate this import mechanism does not allow
+non-standard (.py) file extensions.  Testing does bare this out.
+I have left plug_ext currently in the API, in the off chance that it can
+be enabled in the future.
+
+Once the candidate plugin files are detected, there are several ways to access
+the plugin contents:
+
+1) Indirectly accessing the plugin name space (via get):
+
+.. code-block:
+
+>>>test.get("pyfile1", "__email__")
+'Benjamin@Schollnick.net'
+>>>test.get("pyfile1", "test")("this is an example of calling a function via get")
+test,  this is an example of calling a function via get```
+
+
+2) Or via directly access the plugin name space (via get_plugin).  Please note:
+    the name is based off the file system filename of the module.
+
+.. code-block:
+>>>activeplugin = test.get_plugin("pyfile1")
+>>>activeplugin
+<module 'pyfile1.py' from '....\\PluginMgr\\plugins\\pyfile1.py'>
+>>>activeplugin.__name__
+'pyfile1.py'
+>>>activeplugin.test("hello from Test!")    # running the function test
+test,  hello from Test!
+
 
 .. moduleauthor:: Benjamin Schollnick <Benjamin@Schollnick.net>
 
@@ -52,53 +82,49 @@ from importlib.util import spec_from_file_location, module_from_spec
 
 class PlugInMgr:
     """
-        Locate what files are available as plugins and load them.
+    Plug-In Manager core class
 
-    Cached Exist functionality - Caching engine to detect by filename,
-        and SHA224.   Can use last modification and file / dir count to
-        identify cache invalidation.
-
-    Args:
-
-        reset_count (integer): The number of queries to allow before
-            forcing a cache invalidation.
-
-        use_modify (boolean): Store & Use the last modified date of
-            the contents of the directory for cache invalidation.
-
-        use_shas (boolean): Store & Use SHA224 for the files that
-            are scanned.
-
-        FilesOnly (boolean): Ignore directories
-
-        use_extended (boolean): Store direntries, and break out
-            directory & files counts.
-
-     Returns:
-
-        Boolean: True if the file exists, or false if it doesn't.
-        Integer: If rtn_size is true, an existing file will return
-            an integer
-
-
-    .. code-block:
-        # Boolean Tests
-        >>> file_exist(r"test_samples\\monty.csv")
-        True
-        >>> file_exist(r"test_samples\\small.csv")
-        True
-        >>> file_exist(r"test_samples\\monty_lives_here.csv")
-        False
     """
     def __init__(self,
                  plugin_dir=r".{}Plugins".format(os.sep),
                  allow_creation=False,
                  plug_ext=".py"):
+        """
+            Plug-in Manager.  Locate, Manage, and hand-off Plug-Ins.
 
+        Args:
+
+            plugin_dir (str): The path to the plug-in files
+
+            allow_creation (boolean): Default - False.  Controls the behavior
+                If the plugin_dir does not exist.  If set True, the directory
+                will be created.  If False (Default), then it will silently
+                fail.
+
+            plug_ext (str): The file extension (eg. .py) of the plug-in files.
+                **CURRENTLY UNUSED**  For future use.
+
+        Returns:
+
+            Boolean: True if the file exists, or false if it doesn't.
+            Integer: If rtn_size is true, an existing file will return
+                an integer
+
+
+        Examples
+        --------
+
+        import PyPluginMgr
+        test = PyPluginMgr.PlugInMgr(plugin_dir=r"plugins",
+                                     allow_creation=True)
+        test.findcandidate_files()
+
+        """
         self.plugin_dir = pathlib.Path.cwd() / plugin_dir
         self.candidate_files = []
         self.plug_ext = plug_ext
         self.catalog = {}
+        self.formattedCatalog = {}
 
         if not self.plugin_dir.exists() and allow_creation == True:
             self.plugin_dir.mkdir(exist_ok=True)
@@ -106,56 +132,36 @@ class PlugInMgr:
 
     def findcandidate_files(self):
         """
-            Locate what files are available as plugins and load them.
+        Find the candidate files for the Plug-in Manager.
 
-        Cached Exist functionality - Caching engine to detect by filename,
-            and SHA224.   Can use last modification and file / dir count to
-            identify cache invalidation.
+	    Raises:
 
-        Args:
+            ImportError : If unable to import a plug-in, ImportError will be
+                raised.
 
-            reset_count (integer): The number of queries to allow before
-                forcing a cache invalidation.
+        Examples
+        --------
 
-            use_modify (boolean): Store & Use the last modified date of
-                the contents of the directory for cache invalidation.
-
-            use_shas (boolean): Store & Use SHA224 for the files that
-                are scanned.
-
-            FilesOnly (boolean): Ignore directories
-
-            use_extended (boolean): Store direntries, and break out
-                directory & files counts.
-
-         Returns:
-
-            Boolean: True if the file exists, or false if it doesn't.
-            Integer: If rtn_size is true, an existing file will return
-                an integer
+        import PyPluginMgr
+        test = PyPluginMgr.PlugInMgr(plugin_dir=r"plugins",
+                                     allow_creation=True)
+        test.findcandidate_files()
 
 
-        .. code-block:
-            # Boolean Tests
-            >>> file_exist(r"test_samples\\monty.csv")
-            True
-            >>> file_exist(r"test_samples\\small.csv")
-            True
-            >>> file_exist(r"test_samples\\monty_lives_here.csv")
-            False
-                """
+        """
         #cfiles = list(self.plugin_dir.glob("*{}".format(self.plug_ext)))
         sys.path.append(str(self.plugin_dir))
         cfiles = self.plugin_dir.glob("*{}".format(self.plug_ext))
         self.candidate_files = []
         for x in cfiles:
             self.candidate_files.append(str(x))
-            #base_filename = os.path.splitext(os.path.split(x)[1])[0]
             base_filename = os.path.split(x)[1]
 
             try:
                 module = self.get_installed_config(base_filename)
                 self.catalog[os.path.splitext(base_filename)[0]] = [module,\
+                    dict(inspect.getmembers(module))]
+                self.formattedCatalog[os.path.splitext(base_filename)[0].title()] = [module,\
                     dict(inspect.getmembers(module))]
             except AttributeError:
 
@@ -164,11 +170,10 @@ class PlugInMgr:
             #self.PluginCatlaog
 
 
-    def get_installed_config(self, config_file):#installDir, config_file):
+    def get_installed_config(self, config_file):
         """
-        Reads config from the installation directory of Plenum.
+        import a python file from file system
 
-        #:param installDir: installation directory of Plenum
         :param config_file: name of the configuration file
         :raises: FileNotFoundError
         :return: the configuration as a python object
@@ -188,10 +193,30 @@ class PlugInMgr:
 
     def get_plugin(self, plugin_name):
         """
-        Return the pointer to the name space of the plugin
+        Return module to access the plug-in (or in otherwords return a
+        "Pointer" to the name space of the module)
+
+        Args:
+
+            plugin_name (str): The File System filename (w/o extension)
+                of the plug-in.
+
+
+        Examples
+        --------
+
+        import PyPluginMgr
+        test = PyPluginMgr.PlugInMgr(plugin_dir=r"plugins",
+                                     allow_creation=True)
+        test.findcandidate_files()
+        plugin1 = test.get_plugin("myPlugin1")
+        plugin1.run()
+
         """
         if plugin_name in self.catalog:
             return self.catalog[plugin_name][0]
+        if plugin_name.title() in self.formattedCatalog:
+            return self.formattedCatalog[plugin_name.title()][0]
         return None
 
 
@@ -212,23 +237,29 @@ class PlugInMgr:
 
             The third argument is the default value if the attribute does not exist.
         """
-        #if object_name in self.catalog[plugin_name][1]:
-        if self.has(plugin_name, object_name):
-            return self.catalog[plugin_name][1][object_name]
-
+        pname = None
+        if plugin_name in self.catalog:
+            pname = plugin_name
+        elif plugin_name.title() in self.formattedCatalog:
+            pname = plugin_name.title()
+        print(pname)
+        if pname is None:
+            if object_name in self.catalog[pname][1]:
+                return self.catalog[pname][1][object_name]
         return default_value
 
-    def has(self, plugin_name, object_name):
-        """
-        If the object_name is in the plugin_name's name space,
-        then return True.
 
-        Otherwise, return False in the following conditions
-
-            1) plugin was not found (eg not in catalog)
-            2) object was not found in plugin's name space
-
-        """
-        if plugin_name in self.catalog:
-            return object_name in self.catalog[plugin_name][1]
-        return None
+#     def has(self, plugin_name, object_name):
+#         """
+#         If the object_name is in the plugin_name's name space,
+#         then return True.
+#
+#         Otherwise, return False in the following conditions
+#
+#             1) plugin was not found (eg not in catalog)
+#             2) object was not found in plugin's name space
+#
+#         """
+#         if plugin_name in self.catalog:
+#             return object_name in self.catalog[plugin_name][1]
+#         return None
